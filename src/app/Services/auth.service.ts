3a6fa50c67041cafbecd8ee1f8d7ca127a3031ca
amer-rouby <69314990+amer-rouby@users.noreply.github.com>
2024-10-12 16:57:49 +0300

    User permissions have been added

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -7,12 +7,14 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
+  private userPermissions: boolean = false;
 
   constructor(private afAuth: AngularFireAuth) {
     this.afAuth.setPersistence('session')
       .then(() => {
         this.afAuth.authState.subscribe(user => {
           if (user) {
+            this.loadUserPermissions(user.uid);
             this.setLoginStatus(true);
           } else {
             this.setLoginStatus(false);
@@ -58,4 +60,14 @@ export class AuthService {
   setLoginStatus(isLoggedIn: boolean): void {
     sessionStorage.setItem('isLoggedIn', String(isLoggedIn));
   }
+
+  loadUserPermissions(userId: string): void {
+    this.userPermissions = userId === "W7JBBlBDgmfekGnc6imbK9U9czL2" || userId === "RYxN9sPhcUNmACxmRtgeBcCUQ4h2";
+
+  }
+
+  canRegisterUser(): boolean {
+    return this.userPermissions;
+  }
+  
 }
